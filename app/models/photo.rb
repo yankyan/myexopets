e91f03625e47c17365b810338028a89f1c8f65f3
Sergey <sergey-shestov@yandex.ru>
2017-02-27 14:33:37 +0300

    model_photo_size

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ApplicationRecord
-  has_attached_file :image, styles: {large:"600x600", medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: {large:"600x600>", medium: "360x240#", thumb: "240x180#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   belongs_to :pet
 end
